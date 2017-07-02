55fdf04769e783f6647f2c362dfc0157aa7e1ea7
Kian Ryan <kian@orangetentacle.co.uk>
2017-07-02 23:06:09 +0100

    Change of color picker packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule }   from '@angular/forms';
-import {ColorPickerModule} from 'angular4-color-picker';
+import {ColorPickerModule} from 'ngx-color-picker';
 
 
 import { AppComponent } from './app.component';
